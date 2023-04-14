77a0c65aa1b0caee8a3ad28e861930c443285549
Matthew Brisebois <matthew@webheroes.ca>
2023-04-14 11:27:46 -0600

    Forward @whi/holochain-client exports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const log				= require('@whi/stdlog')(path.basename( __filename ), {
     level: (!__dirname.includes("/node_modules/") && process.env.LOG_LEVEL ) || 'fatal',
 });
 
+const HolochainClientLib		= require('@whi/holochain-client');
+
 const { Holochain,
 	TimeoutError }			= require('./holochain.js');
 const Config				= require('./config.js');
@@ -12,4 +14,5 @@ module.exports = {
     Holochain,
     Config,
     TimeoutError,
+    HolochainClientLib,
 };
